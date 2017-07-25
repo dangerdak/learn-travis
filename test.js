c9be5fc2f3727472001b1faaa916989e03a780eb
Dakshina Scott <dakshina.scott@gmail.com>
2017-07-25 15:39:10 +0100

    Adds test for home route response code

--- test.js
+++ test.js
@@ -6,3 +6,10 @@ test('Initialise', (t) => {
   t.pass('Tape running OK');
   t.end()
 });
+
+test('Home route', (t) => {
+  shot.inject(handler, { method: 'get', url: '/' }, (res) => {
+    t.equal(response.statusCode, 200, 'should respond with status code of 200');
+    t.end();
+  });
+});
